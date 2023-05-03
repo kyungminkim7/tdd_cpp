d88cb3968829c7f093428698a68c70991c88021d
Kyung Kim <kkyung@vmware.com>
2023-05-03 11:22:33 +0000

    Modified Testing CMake module to use main branch of Googletest

--- cmake/module/Testing.cmake
+++ cmake/module/Testing.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.12.1
+    GIT_TAG main
 )
 
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
